01799d53ae2003a4351075518a1642afd86a1a04
WojciechHupert <wojciech.hupert@example.com>
2026-05-10 01:34:41 +0200

    chore: update base path to Moirai_Website

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MoiraiStudio/',
+  base: '/Moirai_Website/',
   plugins: [react()],
   build: {
     rollupOptions: {

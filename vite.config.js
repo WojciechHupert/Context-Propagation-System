dc7fbe9170a22f3a92a48c6df161227180dd43a9
WojciechHupert <wojciech.hupert@example.com>
2026-05-10 07:19:37 +0200

    Update vite config to use relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Moirai_Website/',
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
